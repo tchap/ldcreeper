e7fac5caf11b5247d7f27f745ef7035e0b82870b
Ondrej Kupka <ondra.cap@gmail.com>
2011-08-28 21:23:18 +0200

    Change of source file header

--- model/filter/ModelFilter.java
+++ model/filter/ModelFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Ondrej Kupka <ondra dot cap at gmail dot com>
+ * Copyright (C) 2011 Ondrej Kupka <ondra DOT cap AT gmail DOT com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -21,7 +21,7 @@ import com.hp.hpl.jena.rdf.model.Model;
 
 /**
  *
- * @author Ondrej Kupka <ondra dot cap at gmail dot com>
+ * @author Ondrej Kupka <ondra DOT cap AT gmail DOT com>
  */
 public abstract class ModelFilter {
     
